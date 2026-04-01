3c9fdc7945c9af7c16b3144c1809e03088581e1a
nicoligabrielle <nicaaracoll@gmail.com>
2026-04-01 09:05:49 -0300

    Update index.php

---
+++ lista.php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>intervalo entre 100 e 200</title>
+</head>
+<body>
+
+    <h1>Verificar se o número está entre 100 e 200</h1>
+
+
+    <br>
+    <form name="frmIntervalo" action="ex1.php" method="POST">
+        <label for="txtNumero">Informe um número:</label>
+        <input type="text" name="txtNumero"/>
+        <input type="submit" name="btnCalcular" value="Calcular"/>
+    </form>
+
+</body>
+</html>
