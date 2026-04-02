a52471c9a3c9ba84c281bbe18a6ae1cf3c6b88b8
VictorHsouza20 <victorhsouza2022@gmail.com>
2026-04-02 08:17:16 -0300

    Update index.php

--- lista.php/index.php
+++ lista.php/index.php
@@ -1,3 +1,12 @@
+<!--
+Data: 18/03/2026
+Autor: Nicoli Gabrielle Cunha, Ricardo Amaro Herzig, Suelen Almeida dos Santos Silva e Victor Hugo de Souza Figueiredo
+Objetivo: Criar um programa que apresente um intervalo de números entre 100 e 200
+
+Exercício 1 - Intervalo entre 100 e 200
+Faça um programa que receba um número e diga se este número está no intervalo entre 100 e 200.
+-->
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
