00ffc2fade5480051f4dab84323bd01217f0db7e
nicky <nicaaracoll@gmail.com>
2026-04-02 09:52:58 -0300

    Add metadata comments to temperature converter
    Added metadata comments for program details and objectives.

--- ex2.php/index.php
+++ ex2.php/index.php
@@ -1,3 +1,15 @@
+<!--
+Data: 19/03/2026
+Autor: Nicoli Gabrielle Cunha, Ricardo Amaro Herzig, Suelen Almeida dos Santos Silva
+Objetivo: Criar um programa que converta a temperatura de celcius para fahrenheit ou o contrário
+
+Exercício 2 - Conversão de Temperatura
+Faça um programa que leia um caractere "F" ou "C", indicando se o valor informado está em Fahrenheit ou Celsius.
+Depois, o programa deve converter para a outra unidade.
+
+Fórmula: C = 5/9 × (F − 32)
+-->
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
